d1ce1f7bbcf3c699ea264f0c0d3249d5aee1442e
Jordi <jordicasesnoves@gmail.com>
2020-04-20 19:24:43 +0200

    added zeit now support

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const { ApolloServer, gql, graphqlExpress } = require("apollo-server-express");
+const { typeDefs } = require("./src/typeDefs/index.js");
+const { resolvers } = require("./src/resolvers/index.js");
+
+const PORT = process.env.PORT || 3500;
+const app = express();
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  playground: true,
+  introspection: true,
+});
+
+server.applyMiddleware({ app, path: "/graphql" });
+
+app.listen(PORT, () =>
+  console.log(`🚀 Server ready at http://localhost:${PORT}/graphql`)
+);
