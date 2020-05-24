521ac51b4bcf8a01570652a837922578af60c434
Jordi <jordicasesnoves@gmail.com>
2020-05-24 16:20:04 +0200

    disabled required auth for demo purposes

--- src/resolvers/moviesResolvers.js
+++ src/resolvers/moviesResolvers.js
@@ -3,12 +3,14 @@ const { fetchMovie, fetchMovies } = require("../models/movieModel");
 const moviesResolvers = {
   Query: {
     movies: async (parent, args, context) => {
-      if (!context.user) return null;
+      // Disabled required auth for demo purposes
+      // if (!context.user) return null;
       const { title } = args;
       return fetchMovies({ title });
     },
     movie: async (parent, args, context) => {
-      if (!context.user) return null;
+      // Disabled required auth for demo purposes
+      // if (!context.user) return null;
       const { id } = args;
       return fetchMovie({ id });
     },
--- src/resolvers/showsResolvers.js
+++ src/resolvers/showsResolvers.js
@@ -3,12 +3,14 @@ const { fetchShow, fetchShows } = require("../models/showModel");
 const showsResolvers = {
   Query: {
     shows: async (parent, args, context) => {
-      if (!context.user) return null;
+      // Disabled required auth for demo purposes
+      // if (!context.user) return null;
       const { name } = args;
       return fetchShows({ name });
     },
     show: async (parent, args, context) => {
-      if (!context.user) return null;
+      // Disabled required auth for demo purposes
+      // if (!context.user) return null;
       const { id } = args;
       return fetchShow({ id });
     },
